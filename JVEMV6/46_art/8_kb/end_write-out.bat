98415a4dacd5c084eea8fa15509042638ed9b1e3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-06-30 13:54:12 +0900

    (Cake_IFM11) periodical updates

--- JVEMV6/46_art/8_kb/end_write-out.bat
+++ JVEMV6/46_art/8_kb/end_write-out.bat
@@ -1,6 +1,3 @@
-REM taskkill /im musescore.exe
-taskkill /im MuseScore3.exe
 taskkill /im domino.exe
-taskkill /im filezilla.exe
 taskkill /im wish.exe
 
---
+++ JVEMV6/46_art/8_kb/end_write-out_2.bat
@@ -0,0 +1,6 @@
+REM taskkill /im musescore.exe
+taskkill /im MuseScore3.exe
+taskkill /im domino.exe
+taskkill /im filezilla.exe
+taskkill /im wish.exe
+
