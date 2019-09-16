344508a05459f17bcb0a2a9d6b1d83cbca8c036e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-09-17 08:37:44 +0900

    (Cake_IFM11) periodical updates

---
+++ JVEMV6/46_art/11_guitar/start_guitar.(new).bat
@@ -0,0 +1,31 @@
+REM *******************************
+REM 	open folders
+REM *******************************
+echo start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\11_guitar
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\11_guitar
+
+REM *******************************
+REM 	browser
+REM 	2019/09/16 14:18:11
+REM *******************************
+:web_page_keyboard_opera
+
+REM : opera
+	pushd "C:\WORKS_2\Programs\opera"
+	start launcher.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=gt&RBs_AND_OR_Memo=AND&sort=file_name&direction=desc"
+REM 	start launcher.exe "https://docs.zoho.com/sheet/open/ek7i4683eb5df87d04a23acef09182e7df623/sheets/free/ranges/A3"
+
+	goto exit_web_page_keyboard
+
+
+:exit_web_page_keyboard
+
+REM *******************************
+REM 	doc
+REM 	2019/09/16 14:18:11
+REM *******************************
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\11_guitar\list_of_memos.ods
+
+REM pause
+
+REM exit
