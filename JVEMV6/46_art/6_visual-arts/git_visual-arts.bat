bf97832b2532a752d4c5c22484b4ea38190648d3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-10-28 18:07:40 +0900

    (visual arts) periodical updates

---
+++ JVEMV6/46_art/6_visual-arts/git_visual-arts.bat
@@ -0,0 +1,12 @@
+echo starting...
+
+REM pause
+
+REM C:\WORKS_2\a && cd C:\WORKS_2\WS\WS_Others.Art && git add -A && git commit -m "periodical commits" && git push origin master
+REM C:\WORKS_2\a && cd C:\WORKS_2\WS\WS_Others.Art && git push origin master
+C:\WORKS_2\a && r a && git add . && git commit -m "(visual arts) periodical updates" && git pull https://github.com/iwabuchiken/WS_Others.Art && git push origin master
+
+
+pause
+
+REM exit
--- JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
+++ JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
@@ -1,5 +1,24 @@
+REM ************************
+REM 	folders
+REM 	doc
+REM 	app : gimp
+REM ************************
+
+REM ************************
+REM 	folders
+REM ************************
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts
 
+REM : C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+REM ************************
+REM 	doc
+REM ************************
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\log_46#6.odt
 
+REM ************************
+REM 	app : gimp
+REM ************************
 "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
+
