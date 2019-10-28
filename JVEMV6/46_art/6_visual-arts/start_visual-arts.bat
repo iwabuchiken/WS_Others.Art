f22f0acc6569d6175d316b22e0123b92c7f3c558
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-10-28 09:27:46 +0900

    (Cake_IFM11) periodical updates

---
+++ JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
@@ -0,0 +1,5 @@
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts
+
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\log_46#6.odt
+
+"C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
