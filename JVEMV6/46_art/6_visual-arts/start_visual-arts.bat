71ece2bc5ac3b3517090dc70f960c70c573e17ec
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-10-30 17:46:04 +0900

    (visual arts) periodical updates

--- JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
+++ JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
@@ -15,7 +15,11 @@ start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
 REM ************************
 REM 	doc
 REM ************************
+REM : log
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\log_46#6.odt
+
+REM : sheet
+start 46#6_visual-arts.ods
 
 REM ************************
 REM 	app : gimp
