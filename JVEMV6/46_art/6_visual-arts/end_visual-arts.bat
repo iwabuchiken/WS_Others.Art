d9ca8dcaddf9e6d7e8e87ecdd7e13a3c71fc056f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-10-30 11:17:02 +0900

    (Cake_IFM11/WS_Others.Art) periodical updates

---
+++ JVEMV6/46_art/6_visual-arts/end_visual-arts.bat
@@ -0,0 +1,24 @@
+REM ************************
+REM 	doc
+REM 	app : gimp
+REM 	app : git
+REM ************************
+
+REM ************************
+REM 	doc
+REM ************************
+taskkill /f /im soffice.bin
+
+REM ************************
+REM 	app : gimp
+REM ************************
+taskkill /f /im gimp-2.8.exe
+
+REM ************************
+REM 	app : git
+REM ************************
+taskkill /im wish.exe
+
+
+pause
+
