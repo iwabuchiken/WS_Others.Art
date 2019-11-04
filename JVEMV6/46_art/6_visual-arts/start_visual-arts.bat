2d00466792ea4ed1507e61993348b0b08bd922c8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-11-04 14:18:58 +0900

    (visual arts) periodical updates

--- JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
+++ JVEMV6/46_art/6_visual-arts/start_visual-arts.bat
@@ -2,7 +2,10 @@ REM ************************
 REM 	folders
 REM 	doc
 REM 	app : gimp
+REM 	app : mspaint
 REM ************************
+
+REM goto testing
 
 REM ************************
 REM 	folders
@@ -24,5 +27,15 @@ start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\46#6_visual-arts.o
 REM ************************
 REM 	app : gimp
 REM ************************
+REM :testing
+
 "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
 
+REM call "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
+REM start "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"	==> prog doesn't start; console window opens #20191104_135645
+
+REM ************************
+REM 	app : mspaint
+REM 	2019/11/04 14:10:34
+REM ************************
+start mspaint.exe
