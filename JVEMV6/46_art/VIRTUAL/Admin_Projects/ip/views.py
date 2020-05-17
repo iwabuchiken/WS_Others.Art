5472269456f8be12b8056a4d1a9fa34b04ea8ef8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-05-17 10:38:26 +0900

    (register guitar files) periodical updates

--- JVEMV6/46_art/VIRTUAL/Admin_Projects/ip/views.py
+++ JVEMV6/46_art/VIRTUAL/Admin_Projects/ip/views.py
@@ -2128,29 +2128,29 @@ def prep_gen_Cake_CSV(request):
     '''###################
         open : entries.csv
     ###################'''
-    #command = "C:\\WORKS_2\\WS\\Eclipse_Luna\\Cake_IFM11\\lib\\others\\entries.ods"
-    #command = "start C:\\WORKS_2\\WS\\Eclipse_Luna\\Cake_IFM11\\lib\\others\\entries.ods"
-    command = "C:\\WORKS_2\\WS\\WS_Cake_IFM11\\commands\\1-3)_open-entries-csv.bat"
+#    #command = "C:\\WORKS_2\\WS\\Eclipse_Luna\\Cake_IFM11\\lib\\others\\entries.ods"
+#    #command = "start C:\\WORKS_2\\WS\\Eclipse_Luna\\Cake_IFM11\\lib\\others\\entries.ods"
+#    command = "C:\\WORKS_2\\WS\\WS_Cake_IFM11\\commands\\1-3)_open-entries-csv.bat"
 
-    cmd_Full = [command]  #=> 
-#     cmd_Full = [command, arg1]  #=> 
+#    cmd_Full = [command]  #=> 
+##     cmd_Full = [command, arg1]  #=> 
 
-    print()
-    print("[%s:%d] command => %s" % \
-            (os.path.basename(libs.thisfile()), libs.linenum()
-            , command
-            ), file=sys.stderr)
+#    print()
+#    print("[%s:%d] command => %s" % \
+#            (os.path.basename(libs.thisfile()), libs.linenum()
+#            , command
+#            ), file=sys.stderr)
 
-    '''###################
-        subprocess        
-    ###################'''
-    res = subprocess.call(cmd_Full)
+#    '''###################
+#        subprocess        
+#    ###################'''
+#    res = subprocess.call(cmd_Full)
 
-    print()
-    print("[%s:%d] res => %s" % \
-            (os.path.basename(libs.thisfile()), libs.linenum()
-            , res
-            ), file=sys.stderr)
+#    print()
+#    print("[%s:%d] res => %s" % \
+#            (os.path.basename(libs.thisfile()), libs.linenum()
+#            , res
+#            ), file=sys.stderr)
 
     '''###################
         vars        
