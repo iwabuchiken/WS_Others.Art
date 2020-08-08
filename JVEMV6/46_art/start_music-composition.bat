c6281a556b9627c15b96075328fb2253963b9306
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-09 00:12:21 +0900

    ( compos_musescore : 2020-08-07_17-53-47_000.mscz ) periodical updates

---
+++ JVEMV6/46_art/start_music-composition.bat
@@ -0,0 +1,9 @@
+@echo on
+
+rem ===============================
+rem 	musescore
+rem 	2020”N8ŒŽ8“ú23:36:46
+rem ===============================
+start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
+
+pause
