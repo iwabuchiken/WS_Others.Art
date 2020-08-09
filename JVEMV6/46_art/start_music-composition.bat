a61f03329e6a71a3ef6c7c1b63a13b8a2a6780bc
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-09 23:30:34 +0900

    ( compos_musescore : edit-log_[20200809_230848].txt ) periodical updates

--- JVEMV6/46_art/start_music-composition.bat
+++ JVEMV6/46_art/start_music-composition.bat
@@ -1,6 +1,12 @@
 @echo on
 
 rem ===============================
+rem 	folders
+rem 	2020”N8ŒŽ9“ú22:47:15
+rem ===============================
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\musescore\02_compos
+
+rem ===============================
 rem 	musescore
 rem 	2020”N8ŒŽ8“ú23:36:46
 rem ===============================
