c8a524a397025a00578761d253feb0fb21826280
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-10 16:15:23 +0900

    ( compos_musescore : edit-log_[20200809_230848].txt ) periodical updates

--- JVEMV6/46_art/8_kb/musescore/02_compos/log_[compos_musescore].bat
+++ JVEMV6/46_art/8_kb/musescore/02_compos/log_[compos_musescore].bat
@@ -1,17 +1,4 @@
+@echo on
 
-@@@
-
-================================
-2020年8月9日23:29:25
-
-	edit-log_[20200809_230848].txt ==> new
-
-	next
-		any
-
-================================
-
-
-
-========================= EOF ========================
 C:\WORKS_2\a.bat && r a && git add . && git commit -m "( compos_musescore : edit-log_[20200809_230848].txt ) periodical updates" && e && git push origin master
+
