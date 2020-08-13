cb34d77bdbdb90ad8c0459cbf7a679e1275907e0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-13 15:01:09 +0900

    ( compos_musescore : edit-log_[20200809_230848].txt ) periodical updates

--- JVEMV6/46_art/start_music-composition.bat
+++ JVEMV6/46_art/start_music-composition.bat
@@ -2,16 +2,35 @@
 
 rem ===============================
 rem 	folders
+rem 	log file
+rem 	vlc
+rem 	musescore
+rem ===============================
+
+rem ===============================
+rem 	folders
 rem 	2020年8月9日22:47:15
 rem ===============================
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\musescore\02_compos
 
 rem ===============================
+rem 	log file
+rem 	2020年8月13日13:29:50
+rem ===============================
+C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\musescore\02_compos\log_[compos_musescore].txt"
+
+rem ===============================
+rem 	vlc
+rem 	2020年8月13日13:32:54
+rem ===============================
+start C:\Users\iwabuchiken\VLC\vlc.exe
+
+rem ===============================
 rem 	musescore
 rem 	2020年8月8日23:36:46
 rem ===============================
-rem start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
+start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
 
-start C:\WORKS_2\Programs\musescore\musescore_2.3.2\bin\MuseScore.exe
+rem start C:\WORKS_2\Programs\musescore\musescore_2.3.2\bin\MuseScore.exe
 
 pause
