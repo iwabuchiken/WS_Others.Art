36da8b87db7902287223174cdc90a296e31920e5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-07 17:34:22 +0900

    (res : JVE_46# : compositions) periodical updates / 2020-09-25_08-15-55_000.mp4 2020-09-28_22-06-28_000.mp4.[inst recorder].mscz 2020-10-07_08-16-20_000.mp4.[inst kb].[relay-3].mscz

--- JVEMV6/46_art/13_compositions/end_13_compositions.bat
+++ JVEMV6/46_art/13_compositions/end_13_compositions.bat
@@ -1,14 +1,23 @@
-set msg="%1"
+@echo off
 
+rem set msg="%1 %2 %3 %4 %5"
+set msg=%1 %2 %3 %4 %5
+
+echo.
 echo msg is %msg%
+
+
+echo.
+
+r a && git add -A && git commit -m "(res : JVE_46# : compositions) periodical updates / %msg%" && e && p"
 
 rem echo "C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p"
 
+rem echo %msg%
 
+rem pause
 
-pause
-
-C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p"
+rem C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p"
 
 rem C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p
 
