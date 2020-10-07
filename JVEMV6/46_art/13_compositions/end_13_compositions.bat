36b62f0d929c71525445a5246a81b57c6e1923d3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-07 16:34:42 +0900

    (res : JVE_46# : compositions) periodical updates / 2020-09-25_08-15-55_000.mp4

--- JVEMV6/46_art/13_compositions/end_13_compositions.bat
+++ JVEMV6/46_art/13_compositions/end_13_compositions.bat
@@ -1,3 +1,15 @@
-C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates^" && e && p
+set msg="%1"
+
+echo msg is %msg%
+
+rem echo "C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p"
+
+
+
+pause
+
+C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p"
+
+rem C:\WORKS_2\a.bat && r a && git add -A && git commit -m ^"(res : JVE_46# : compositions) periodical updates / %msg%^" && e && p
 
 pause
