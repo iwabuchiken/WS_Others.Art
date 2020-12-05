6303f3a4aab95ce18a6c759d081c6e5d404753b6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-05 17:20:45 +0900

    (46_6.5.2 : log_[5_free-painting]) periodical updates

--- JVEMV6/46_art/8_kb/start_keyboard.bat
+++ JVEMV6/46_art/8_kb/start_keyboard.bat
@@ -40,11 +40,25 @@ REM ==============================
 :web_page_keyboard_opera
 
 REM : opera
-	pushd "C:\WORKS_2\Programs\opera"
+pushd "C:\WORKS_2\Programs\opera"
+
+rem : set urls 2020年11月17日18:27:33
+rem : log files / 2020年11月17日18:27:39
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=music+log&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+
+rem : video files : bamboo / 2020年11月17日18:27:39
+set url_2="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=video+bamboo&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+
+rem : video files : flute / 2020年11月17日18:39:02
+set url_2="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=video+jap.flute&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+
+start launcher.exe %url_1%
+start launcher.exe %url_2%
+
 REM : log
-	start launcher.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=kb+log&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+rem 	start launcher.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=kb+log&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 REM : video
-	start launcher.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=video+kb&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+rem 	start launcher.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=video+kb&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 REM 	start launcher.exe "https://docs.zoho.com/sheet/open/ek7i4683eb5df87d04a23acef09182e7df623/sheets/free/ranges/A3"
 
 rem 	start launcher.exe "https://www.youtube.com/playlist?list=PLZe1Q2NRG8YXa2FCHsE3Ji3VCOg2Ey8LS"
@@ -83,8 +97,9 @@ REM ==============================
 REM 	mind map
 REM 	2020年7月20日18:35:01
 REM ==============================
-rem pushd "C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\46 Art\8 kb"
-rem start 3_music-types.mm
+rem : comment in 2020年10月25日18:13:20
+pushd "C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\46 Art\8 kb"
+start 3_music-types.mm
 
 rem : comment in 2020年10月21日19:08:01
 start "C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\46 Art\8 kb\3_music-types.mm"
