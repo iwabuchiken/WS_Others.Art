8eb22f8be21d24dc98e54299db7e7ea0b28e6e53
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-15 14:29:32 +0900

    ( jve_[46#13].[theme=compositions] ) periodical updates

--- JVEMV6/46_art/8_kb/musescore/02_compos/end_[compos_musescore].bat
+++ JVEMV6/46_art/8_kb/musescore/02_compos/end_[compos_musescore].bat
@@ -1,4 +1,7 @@
 @echo on
 
-C:\WORKS_2\a.bat && r a && git add . && git commit -m "( compos_musescore : edit-log_[2020-09-25_08-15-55_000.mp4.mscz].txt ) periodical updates" && e && git push origin master
+rem C:\WORKS_2\a.bat && r a && git add . && git commit -m "( compos_musescore : edit-log_[2020-09-25_08-15-55_000.mp4.mscz].txt ) periodical updates" && e && git push origin master
+
+
+C:\WORKS_2\a.bat && r a && git add . && git commit -m "( jve_[46#13].[theme=compositions] ) periodical updates" && e && p && r f && r a && g a . && g c -m "( jve_[46#13].[theme=compositions] ) periodical updates" && e && p
 
