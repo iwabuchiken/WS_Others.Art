0c5e3d6d55f951f375da2ec82279a170551681e4
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-02-15 12:18:23 +0900

    ( jve_[46#13].[theme=compositions] ) periodical updates

--- JVEMV6/46_art/8_kb/compos/end_[compos].bat
+++ JVEMV6/46_art/8_kb/compos/end_[compos].bat
@@ -1,8 +1,8 @@
 @echo on
 
-tasklist /f /im javaw.exe
-tasklist /f /im sakura.exe
-tasklist /f /im vlc.exe
+taskkill /f /im javaw.exe
+taskkill /f /im sakura.exe
+taskkill /f /im vlc.exe
 
 
 C:\WORKS_2\a.bat && r a && git add . && git commit -m "( jve_[46#13].[theme=compositions] ) periodical updates" && e && p
